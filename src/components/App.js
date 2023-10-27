175f7aa5974383a2a686aba5956a08222cc8d53d
Victor <victormakanda254@gmail.com>
2023-10-28 00:06:06 +0300

    First Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,38 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+
+  const [tasks, setTasks]= useState(TASKS)
+  const categories= CATEGORIES
+  const [selectedCategory, setSelectedCategory] = useState("All")
+
+  function onDelete(text){
+    const newTasks = tasks.filter((task) => (task.text !== text))
+    setTasks(newTasks)
+  }
+
+  function addNewTask(newTask){
+    setTasks([...tasks, newTask])
+  }
+
+  
+
+
+    const tasksToDisplay= tasks.filter((task) => {
+      if(selectedCategory === "All"){
+        return true
+      } else{
+        return selectedCategory === task.category
+      }
+  })
+
+  
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={categories} onButton={selectedCategory} selectedCategory={setSelectedCategory}/>
+      <NewTaskForm categories={categories} onTaskFormSubmit={addNewTask}/>
+      <TaskList tasks={tasksToDisplay} onDelete={onDelete}/>
     </div>
   );
 }
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 
-function NewTaskForm() {
+function NewTaskForm({categories, onTaskFormSubmit}) {
+
+  const [details, setDetails] = useState();
+  const [selectOption, setSelectOption] = useState()
+  
+
+  const optionCategories= categories.filter(category => category !== "All")
+  
+  function handleDetails(e){
+    setDetails(e.target.value)
+    //console.log(details)
+  }
+  function handleOptions(e){
+    setSelectOption(e.target.value)
+  }
+
+  function handleSubmit(e){
+     e.preventDefault();
+     const formData = {text : details, category: selectOption}
+     onTaskFormSubmit(formData)
+  }
+
+  
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" onChange={(e)=> handleDetails(e)} value={details}/>
       </label>
       <label>
         Category
-        <select name="category">
+        <select name="category" onChange={handleOptions}>
           {/* render <option> elements for each category here */}
+          {
+            optionCategories.map((category => <option key={category} value={selectOption}>{category}</option>))
+          }
+
         </select>
       </label>
       <input type="submit" value="Add task" />
